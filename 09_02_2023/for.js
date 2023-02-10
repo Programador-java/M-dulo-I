c5bf14fe952a18b6746fb297f7f660d9212e9aaf
Programador-java <124096745+Programador-java@users.noreply.github.com>
2023-02-10 09:21:16 -0300

    Add files via upload

---
+++ 09_02_2023/for.js
@@ -0,0 +1,8 @@
+    // 2. Construir um programa no qual o 
+// usuário digitará um número e o programa fará a
+// tabuada de multiplicação deste número.
+
+/* for( var n=0; n<11; n++) {
+    console.log('5x' + n + '=' + 5*n);
+}
+ */
