f9dfa6e99921073bbd2c2e70b4dfafef8af3ac92
Programador-java <124096745+Programador-java@users.noreply.github.com>
2023-02-10 09:18:37 -0300

    Add files via upload

---
+++ 10_02_2023/script.js
@@ -0,0 +1,23 @@
+
+// var numero1 = 2;
+// var numero2 = 4;
+// var operacao = numero1 * numero2;
+// console.log(`A multiplicação entre ${numero1} e ${numero2} é ${operacao}`);
+
+/* function operacao(numero1, numero2) {
+    return numero1 * numero2
+};
+
+console.log(operacao(3,4));
+console.log(operacao(2,5)); */
+
+// Construir um programa no qual o usuário irá digitar dois 
+// números e o programa irá calcular a soma destes números 
+// (deverá utilizar função para que o programa escreva a 
+//    saída do programa).
+
+function soma(n1, n2) {
+    return n1 + n2
+};
+
+console.log(soma(3, 4));
